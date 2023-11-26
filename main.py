7e38f0468eaa285c01e682509268a79ebe4ef111
Tian Welgemoed <tian.welgemoed@gmail.com>
2023-11-26 15:05:56 +1300

    use env file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import asyncio
-from pytube import YouTube
-from discord import FFmpegPCMAudio
 import os
 import discord
+import asyncio
+from dotenv import load_dotenv
+from pytube import YouTube
+from discord import FFmpegPCMAudio
 from discord.ext import commands
 
+load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -85,4 +87,4 @@ async def play_sound(ctx, name):
         print(exception)
         await ctx.send("An error occurred while processing your request.")
 
-bot.run("")
+bot.run(os.getenv("TOKEN"))
